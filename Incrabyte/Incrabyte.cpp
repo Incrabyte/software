b5f26154576d5aeaed1d3bf5cb3a13487c5f579f
p0tato-stonks <isaacgoldberg6@gmail.com>
2022-07-26 18:33:33 -0700

    Add project files.

---
+++ Incrabyte/Incrabyte.cpp
@@ -0,0 +1,12 @@
+﻿// Incrabyte.cpp : Defines the entry point for the application.
+//
+
+#include "Incrabyte.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
